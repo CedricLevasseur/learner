36ec10ba73568c52a14a5306c779d5bb6179b3c0
Cédric Levasseur <cedric.levasseur@gmail.com>
2020-04-19 15:56:43 +0200

    Format

--- src/main/java/com/cedriclevasseur/guitar/learner/QuizzDegree.java
+++ src/main/java/com/cedriclevasseur/guitar/learner/QuizzDegree.java
@@ -9,16 +9,16 @@ public class QuizzDegree {
 
     public static void main(String[] args) {
 
-Scale scale = Scale.of(Note.C);
-System.out.println("Scale of C : " + scale);
+        Scale scale = Scale.of(Note.C);
+        System.out.println("Scale of C : " + scale);
 
-Scale mode = Mode.IONIAN.of(scale);
-System.out.println("Scale of C in "+Mode.IONIAN+" mode : " + mode);
+        Scale mode = Mode.IONIAN.of(scale);
+        System.out.println("Scale of C in " + Mode.IONIAN + " mode : " + mode);
 
-Scale chord = Chord.major(mode);
-System.out.println("Major chord : " + chord);
+        Scale chord = Chord.major(mode);
+        System.out.println("Major chord : " + chord);
 
-Guitar.display(chord, Guitar.STANDART_TUNING, 12);
+        Guitar.display(chord, Guitar.STANDART_TUNING, 12);
     }
 
 }
